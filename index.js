fa70f99068ba445c7348593f77a6489300efe729
kchapelier <kevin.chapelier@gmail.com>
2014-03-16 14:24:16 +0100

    place default operators in another file

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-var DataFilter = require('./lib/datafilter.js');
+var DataFilter = require('./lib/datafilter.js'),
+    operators = require('./lib/operators.js');
+
+operators(DataFilter); //inject default operators
 
 module.exports = DataFilter;
