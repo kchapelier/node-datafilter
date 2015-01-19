8f5347ebfb1257fb6f81b6e5d50eb4fce1c54aaa
kchapelier <kevin.chapelier@gmail.com>
2015-01-19 13:20:28 +0100

    update deps, setup jshint and jscs

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var gulp = require('gulp'),
+    jscs = require('gulp-jscs'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish');
+
+var files = ['./index.js', './lib/*.js', './gulpfile.js'];
+
+gulp.task('lint', function () {
+    return gulp
+        .src(files)
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('codestyle', function () {
+    return gulp
+        .src(files)
+        .pipe(jscs());
+});
